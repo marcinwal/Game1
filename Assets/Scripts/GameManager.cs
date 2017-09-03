8a9f4b4a8a819203faa16649f4a5ed6a524daee3
marcinwal <marcin.walendzik@gmail.com>
2017-09-03 14:50:30 +0200

    1st commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance; //singleton
+    public GameState currentGameState = GameState.menu;
+
+    void Awake()
+    {
+        instance = this;
+    } 
+
+    void Start()
+    {
+        //StartGame();
+        currentGameState = GameState.menu;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("s"))
+        {
+            StartGame();
+        }
+    }
+
+    // Use this for initialization
+    public void StartGame () {
+        PlayerController.instance.StartGame();
+        SetGameState(GameState.inGame);
+	}
+
+    // Update is called once per frame
+    public void GameOver () {
+        SetGameState(GameState.gameOver);
+	}
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        if( newGameState == GameState.menu)
+        {
+
+        } else if ( newGameState == GameState.inGame) {
+
+        } else if ( newGameState == GameState.gameOver)
+        {
+
+        }
+
+        currentGameState = newGameState;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float jumpForce = 25f;
+    public float runningSpeed = 1.5f;
+    private Rigidbody2D rigidBody;
+    public static PlayerController instance;
+    private Vector3 startingPosition;
+
+    public LayerMask groundLayer;
+    public Animator animator;
+
+    private void Awake()
+    {
+        instance = this;
+        rigidBody = GetComponent<Rigidbody2D>();
+        startingPosition = this.transform.position;
+    }
+    // Use this for initialization
+    public void StartGame () {
+        animator.SetBool("isAlive", true);
+        this.transform.position = startingPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Jump();
+            }
+            animator.SetBool("isGrounded", IsGrounded());
+        }
+	}
+
+
+    void Jump()
+    {
+        if (IsGrounded())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (rigidBody.velocity.x < runningSpeed)
+            {
+                rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+            }
+        }
+    }
+
+    bool IsGrounded()
+    {
+        if(Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value))
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public void Kill()
+    {
+        GameManager.instance.GameOver();
+        animator.SetBool("isAlive", false);
+    }
+}
